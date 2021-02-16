f49eec7f1e4489f6711466b671d612795736982e
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-16 17:02:57 -0500

    Adds fetchEventsFromServer method with initial basic network call. Still needs to see what the call returns to add decode method to model and add auth to the request.

--- EventsSeatGeek/EventsSeatGeek/Controllers/EventController.swift
+++ EventsSeatGeek/EventsSeatGeek/Controllers/EventController.swift
@@ -10,4 +10,32 @@ import Foundation
 class EventController {
     // MARK: - Properties
     var events: [Event]?
+    let baseUrl: URL = URL(string: "https://api.seatgeek.com/2")!
+    typealias CompletionHandler = (Error?) -> Void
+
+    // MARK: - REST methods
+    func fetchEventsFromServer(completion: @escaping CompletionHandler = { _ in }) {
+        let requestURL = baseUrl.appendingPathExtension("events")
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching events: \(error)")
+                completion(error)
+                return
+            }
+
+            guard let data = data else {
+                NSLog("No data returned by dataTask")
+                completion(NSError())
+                return
+            }
+
+            do {
+                self.events = try JSONDecoder().decode([Event].self, from: data)
+
+            } catch {
+                NSLog("Error decoding or saving data from SeatGeek: \(error)")
+                completion(error)
+            }
+        }
+    }
 }
