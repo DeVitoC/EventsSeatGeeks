1cc0acb02be3b7ecfa260026be879405001590bc
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-16 14:55:59 -0500

    Adds model for Event with image, title, city, state, and state properties.

---
+++ EventsSeatGeek/EventsSeatGeek/Models/Event.swift
@@ -0,0 +1,16 @@
+//
+//  Event.swift
+//  EventsSeatGeek
+//
+//  Created by Christopher Devito on 2/16/21.
+//
+
+import UIKit
+
+struct Event {
+    let image: URL
+    let title: String
+    let city: String
+    let state: String
+    let date: Date
+}
