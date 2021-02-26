69c3982b9489d3a62389dc8bf92c61eaaf168c17
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-26 10:46:13 -0500

    Testing gitignore

--- EventsSeatGeek/EventsSeatGeek/Application/Constants.swift
+++ EventsSeatGeek/EventsSeatGeek/Application/Constants.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct Constants {
     static var API_KEY = ""
-    static var CLIENT_ID = ""
+    static var CLIENT_ID = "test"
     static var API_URL = "https://api.seatgeek.com/2/events?client_id=\(CLIENT_ID)&client_secret=\(API_KEY)"
 }
