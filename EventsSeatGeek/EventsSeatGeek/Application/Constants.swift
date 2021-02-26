bf803076b17166adcc77cdc920806f451178a121
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-26 11:00:31 -0500

    Create Constants.swift

---
+++ EventsSeatGeek/EventsSeatGeek/Application/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  EventsSeatGeek
+//
+//  Created by Christopher Devito on 2/26/21.
+//
+import Foundation
+
+struct Constants {
+    static var API_KEY = "testing one last time"
+    static var CLIENT_ID = "testing one last time"
+    static var API_URL = "https://api.seatgeek.com/2/events?client_id=\(CLIENT_ID)&client_secret=\(API_KEY)"
+}
