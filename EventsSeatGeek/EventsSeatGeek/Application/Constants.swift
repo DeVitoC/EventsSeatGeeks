5262baac9f333276bd5c3f9111a11e89adb2d078
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-26 10:57:08 -0500

    testing git ignore again

--- EventsSeatGeek/EventsSeatGeek/Application/Constants.swift
+++ EventsSeatGeek/EventsSeatGeek/Application/Constants.swift
@@ -7,7 +7,7 @@
 import Foundation
 
 struct Constants {
-    static var API_KEY = "testing one last time"
-    static var CLIENT_ID = "testing one last time"
+    static var API_KEY = "testing"
+    static var CLIENT_ID = "testing"
     static var API_URL = "https://api.seatgeek.com/2/events?client_id=\(CLIENT_ID)&client_secret=\(API_KEY)"
 }
