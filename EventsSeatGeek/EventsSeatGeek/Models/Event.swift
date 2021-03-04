74ea1ad95e326b28dcfa36c1f79172de94eb32d4
Christopher DeVito <christopher.devito@protonmail.com>
2021-03-04 15:37:07 -0500

    Simplified decoding by adding structs for Events, Venue, and Performers.

--- EventsSeatGeek/EventsSeatGeek/Models/Event.swift
+++ EventsSeatGeek/EventsSeatGeek/Models/Event.swift
@@ -8,45 +8,17 @@
 import UIKit
 
 struct Event: Decodable {
-    let image: String
+    let performers: [Performer]
     let title: String
-    let city: String
-    let state: String
-    let date: Date
+    let venue: Venue
+    let datetime_local: String
+}
 
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
+struct Venue: Decodable {
+    var city: String
+    var state: String
+}
 
-        let date = try container.decode(Date.self, forKey: .date)
-
-        let venueContainer = try container.nestedContainer(keyedBy: VenueCodingKeys.self, forKey: .venue)
-        let image = try venueContainer.decode(String.self, forKey: .image)
-        let city = try venueContainer.decode(String.self, forKey: .city)
-        let state = try venueContainer.decode(String.self, forKey: .state)
-
-        let performersContainer = try container.nestedContainer(keyedBy: PerformersCodingKeys.self, forKey: .performers)
-        let title = try performersContainer.decode(String.self, forKey: .title)
-
-        self.image = image
-        self.title = title
-        self.city = city
-        self.state = state
-        self.date = date
-    }
-
-    enum CodingKeys: String, CodingKey {
-        case date = "datetime_utc"
-        case venue
-        case performers
-    }
-
-    enum VenueCodingKeys: String, CodingKey {
-        case image
-        case city
-        case state
-    }
-
-    enum PerformersCodingKeys: String, CodingKey {
-        case title = "name"
-    }
+struct Performer: Decodable {
+    var image: String
 }
---
+++ EventsSeatGeek/EventsSeatGeek/Models/Events.swift
@@ -0,0 +1,12 @@
+//
+//  Events.swift
+//  EventsSeatGeek
+//
+//  Created by Christopher Devito on 3/4/21.
+//
+
+import Foundation
+
+struct Events: Decodable {
+    var events: [Event]
+}
