67f53a7a72420de4f1729650b4f732f3e175784f
Christopher DeVito <christopher.devito@protonmail.com>
2021-03-10 13:30:24 -0500

    Enabled searching in the EventsViewController. Added currentListOfEvents, isSearching, and UISearchBarDelegate functionality.

--- EventsSeatGeek/EventsSeatGeek/Views/EventsViewController.swift
+++ EventsSeatGeek/EventsSeatGeek/Views/EventsViewController.swift
@@ -11,6 +11,8 @@ class EventsViewController: UIViewController {
 
     // MARK: - Properties
     var eventController = EventController()
+    var currentListOfEvents: [Event]?
+    var isSearching: Bool = false
 
     // MARK: - Outlets
     @IBOutlet weak var eventsTableView: UITableView!
@@ -22,9 +24,11 @@ class EventsViewController: UIViewController {
 
         eventsTableView.delegate = self
         eventsTableView.dataSource = self
+        searchBar.delegate = self
 
         eventController.fetchEventsFromServer { (events, error) in
             DispatchQueue.main.async {
+                self.currentListOfEvents = events
                 self.eventsTableView.reloadData()
             }
         }
@@ -49,14 +53,58 @@ extension EventsViewController: UITableViewDelegate {
 
 extension EventsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        eventController.events?.count ?? 0
+        currentListOfEvents?.count ?? 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath) as? EventTableViewCell else { return UITableViewCell() }
-        cell.event = eventController.events?[indexPath.row]
+        cell.event = currentListOfEvents?[indexPath.row]
 
         return cell
     }
 
+}
+
+extension EventsViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        isSearching = true
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        isSearching = false
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            currentListOfEvents = eventController.events
+            eventsTableView.reloadData()
+            return
+        }
+        guard let events = eventController.events else { return }
+        var newListOfEvents: [Event] = []
+
+        for event in events {
+            if event.searchText.contains(searchText.lowercased()) {
+                newListOfEvents.append(event)
+            }
+        }
+
+        if newListOfEvents.count == 0 {
+            isSearching = false
+        } else {
+            isSearching = true
+        }
+
+        currentListOfEvents = newListOfEvents
+
+        eventsTableView.reloadData()
+    }
 }
